77dffd06f2999e3ec43dc87b4ee63af5f3fb9e88
Mishu <mishudebnath927@gmail.com>
2026-03-06 20:21:23 +0600

    login page functionality add complite

--- script/login.js
+++ script/login.js
@@ -1,3 +1,20 @@
 
 // global variable
 
+const usernameInput = document.getElementById('username');
+const passwordInput = document.getElementById('password');
+const loginBtn = document.getElementById('login-btn');
+
+loginBtn.addEventListener('click', () => {
+
+    if (usernameInput.value !== 'admin') {
+        alert('invalid username');
+        return;
+    } else if (passwordInput.value !== 'admin123') {
+        alert('invalid password');
+        return;
+    } else {
+        alert('Login Sessusfull');
+        location.assign('main.html');
+    }
+});
