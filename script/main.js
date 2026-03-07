d2ce3a331aedd0bc5386b941e948f0bdea2442d9
Mishu <mishudebnath927@gmail.com>
2026-03-07 13:13:15 +0600

    open and closed issue add and issue status image dynamicaly add functionality add complite

--- script/main.js
+++ script/main.js
@@ -29,18 +29,24 @@ const allIssuesShow = async () => {
         let date = new Date(item.createdAt).toLocaleDateString();
 
         const card = document.createElement('div');
+
         card.classList.add('card', 'bg-base-100', 'shadow-sm', 'w-auto', 'border-t-5');
         if (item.status === 'open') {
             card.classList.add('border-green-700');
+            statusImg= './assets/Open-Status.png'
         } else {
             card.classList.add('border-purple-700');
+            statusImg = './assets/Closed- Status .png'
         }
 
         card.innerHTML = `
             <div class="p-5 space-y-3 border-b border-gray-300">
 
-                <div class="card-actions justify-between">
-                    <img src="./assets/Open-Status.png" alt="Open Status">
+                <div  class=" card-actions justify-between">
+                
+                    <div>
+                        <img class="issue-status" src="${statusImg}" alt="Open Status">
+                    </div>
 
                     <div class="badge  bg-[#FEECEC]  text-[#EF4444] rounded-full uppercase font-semibold"> ${item.priority}</div>
                 </div>
@@ -76,6 +82,140 @@ const allIssuesShow = async () => {
 
 }
 
+// open Issues show
+const openIssuesShow = async () => {
+    const res = await fetch('https://phi-lab-server.vercel.app/api/v1/lab/issues');
+    const data = await res.json();
+    const allIsue = data.data;
+    let openCardCount = 0;
+    console.log(openCardCount);
+
+    issuesNumberCountElement.textContent = `0 Issues`;
+
+
+    // allissus per item get
+    allIsue.map(item => {
+        console.log(item);
+        // console.log((item.status === 'open').length);
+        let date = new Date(item.createdAt).toLocaleDateString();
+
+        const card = document.createElement('div');
+        card.classList.add('card', 'bg-base-100', 'shadow-sm', 'w-auto', 'border-t-5');
+        if (item.status === 'open') {
+            card.classList.add('border-green-700');
+             statusImg = './assets/Open-Status.png'
+        }
+
+
+        card.innerHTML = `
+            <div class="p-5 space-y-3 border-b border-gray-300">
+
+                <div class="card-actions justify-between">
+                    <img src="${statusImg}" alt="Open Status">
+
+                    <div class="badge  bg-[#FEECEC]  text-[#EF4444] rounded-full uppercase font-semibold"> ${item.priority}</div>
+                </div>
+
+                <h3 onclick="modalShow(${item.id})" class="text-[16px] mt-2 capitalize primary-color font-semibold hover:cursor-pointer">${item.title}</h3>
+                <p class="secondary-color">${item.description}</p>
+
+                <div class="card-actions justify-start ">
+
+                   ${item.labels.map(lebel => {
+            return `
+                    <div class="badge border border-gray-200 bg-[#FEECEC]  text-[#EF4444] rounded-full uppercase py-4 font-semibold">
+                        <i class="fa-solid fa-bug"></i> ${lebel}
+                    </div>
+                    
+                         `
+        }).join('')}
+
+                    
+                    
+
+                </div>
+
+            </div>
+
+            <div class="mt-5 px-5 space-y-1 mb-5">
+                <p class="secondary-color">${item.author}</p>
+                <p class="secondary-color">${date}</p>
+            </div>
+        `;
+        if (item.status === 'open') {
+            openCardCount++;
+            issuesCardContainerElement.append(card);
+        }
+    });
+
+    // console.log(issuesCardContainerElement.length);
+}
+
+// close Issues show
+const closeIssuesShow = async () => {
+    const res = await fetch('https://phi-lab-server.vercel.app/api/v1/lab/issues');
+    const data = await res.json();
+    const allIsue = data.data;
+    let openCardCount = 0;
+    console.log(openCardCount);
+
+    issuesNumberCountElement.textContent = `0 Issues`;
+
+    // allissus per item get
+    allIsue.map(item => {
+        let date = new Date(item.createdAt).toLocaleDateString();
+
+        const card = document.createElement('div');
+        card.classList.add('card', 'bg-base-100', 'shadow-sm', 'w-auto', 'border-t-5');
+        if (item.status === 'closed') {
+            card.classList.add('border-purple-700');
+            statusImg = './assets/Closed- Status .png'
+        }
+
+
+        card.innerHTML = `
+            <div class="p-5 space-y-3 border-b border-gray-300">
+
+                <div class="card-actions justify-between">
+                    <img src="${statusImg}" alt="Open Status">
+
+                    <div class="badge  bg-[#FEECEC]  text-[#EF4444] rounded-full uppercase font-semibold"> ${item.priority}</div>
+                </div>
+
+                <h3 onclick="modalShow(${item.id})" class="text-[16px] mt-2 capitalize primary-color font-semibold hover:cursor-pointer">${item.title}</h3>
+                <p class="secondary-color">${item.description}</p>
+
+                <div class="card-actions justify-start ">
+
+                   ${item.labels.map(lebel => {
+            return `
+                    <div class="badge border border-gray-200 bg-[#FEECEC]  text-[#EF4444] rounded-full uppercase py-4 font-semibold">
+                        <i class="fa-solid fa-bug"></i> ${lebel}
+                    </div>
+                    
+                         `
+        }).join('')}
+
+                    
+                    
+
+                </div>
+
+            </div>
+
+            <div class="mt-5 px-5 space-y-1 mb-5">
+                <p class="secondary-color">${item.author}</p>
+                <p class="secondary-color">${date}</p>
+            </div>
+        `;
+        if (item.status === 'closed') {
+            openCardCount++;
+            issuesCardContainerElement.append(card);
+        }
+    });
+
+    // console.log(issuesCardContainerElement.length);
+}
 
 
 // modalShow function
@@ -84,7 +224,6 @@ const modalShow = async (id) => {
     issueDetailModalElement.showModal();
 
     const modalContent = document.getElementById('modal-content');
-    // modalContentElement.innerHTML = '';
 
 
     const res = await fetch(`https://phi-lab-server.vercel.app/api/v1/lab/issue/${id}`);
@@ -110,13 +249,13 @@ const modalShow = async (id) => {
 
                         <div class="card-actions justify-start ">
                             ${modalDetail.labels.map(lebel => {
-                         return `
+        return `
                                     <div class="badge border border-gray-200 bg-[#FEECEC]  text-[#EF4444] rounded-full uppercase py-4 font-semibold">
                                       <i class="fa-solid fa-bug"></i> ${lebel}
                                     </div>
                     
                          `
-                     }).join('')}
+    }).join('')}
 
                         </div>
                         
@@ -146,21 +285,23 @@ const modalShow = async (id) => {
 
 
 
-
 // all button event lissener
 allIssuesBtnElement.addEventListener('click', () => {
-    console.log('clicked');
+    issuesCardContainerElement.innerHTML = '';
+    issuesNumberCountElement.innerHTML = 0;
     allIssuesShow();
 });
 
 openIssuesBtnElement.addEventListener('click', () => {
     issuesCardContainerElement.innerHTML = '';
     issuesNumberCountElement.innerHTML = 0;
+    openIssuesShow()
 });
 
 closeIssuesBtnElement.addEventListener('click', () => {
     issuesCardContainerElement.innerHTML = '';
     issuesNumberCountElement.innerHTML = 0;
+    closeIssuesShow();
 });
 
 
